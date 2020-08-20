20eccdec29a91fb808eab9871a411e1644332fee
KHATU <NGUYENBAOKHATU@GMAIL.com>
2020-08-20 20:24:34 +0700

    fix test and test controller new

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,27 +2,56 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
 
-  describe "GET #create" do
-    it "returns http success" do
-      post :create, params: { category: {name:'laptop'}}
-      expect(Category.count).to eql(1)
-    end
-
-    it "returns http success" do
-      post :create, params: { category: {name:'laptop'}}
-      expect(response).to have_http_status(302)
-    end
+describe "#index" do
+  it "assigns categories" do
+    Category.create(name: 'Macbook')
+    get :index
+    categories = assigns(:categories)
+    expect(categories.size).to eql(1)
   end
 
-  describe "GET #index" do
-    it "populates an array of categories" do
-      get :index, params: { category: {name:'laptop'}}
-      expect(assigns(:categories))
+   it "renders the index view" do
+    get :index
+    expect(response).to render_template(:index)
+  end
+end
+
+describe "#new" do
+  it "assigns a new category to @categories" do
+    get :new
+    expect(response).to have_http_status(:success)
+  end
+
+   it "renders the new view" do
+    get :new
+    expect(response).to render_template(:new)
+  end
+end
+
+  describe "#create" do
+    context "success" do
+      it "create a presisted category" do
+        post :create, params: { category: {name:'Macbook'}}
+        expect(Category.count).to eql(1)
+      end
+
+      it "redirect_to categories index pages" do
+        post :create, params: { category: {name:'Macbook'}}
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(categories_path)
+      end
     end
 
-     it "renders the :index view" do
-      get :index
-      expect(response).to render_template(:index)
+    context "failed" do
+      it "does not create a new category in database" do
+        post :create, params: { category: { name: ''}}
+        expect(Category.count).to eql(0)
+      end
+
+      it "render categories new pages" do
+        post :create, params: { category: { name: ''}}
+        expect(response).to render_template(:new)
+      end
     end
   end
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,30 +2,61 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
 
-  describe "GET #create" do
-    it "returns http success" do
-      category = Category.create name: 'Laptop'
-      post :create, params: { product: { product_name: 'Macbook 2015', quantity: 50, unit_price: 200, category_id: category.id}}
-      expect(Product.count).to eql(1)
-    end
-
-    it "returns http success" do
-      category = Category.create name: 'Laptop'
-      post :create, params: { product: { product_name: 'Macbook 2015', quantity: 50, unit_price: 200, category_id: category.id}}
-      expect(response).to have_http_status(302)
-    end
+describe "#index" do
+  it "assigns products" do
+    category = Category.create(name: 'Laptop')
+    Product.create(product_name: 'Macbook 2015', quantity: 50, unit_price: 200, category_id: category.id)
+    get :index
+    products = assigns(:products)
+    expect(products.size).to eql(1)
   end
 
-  describe "GET #index" do
-    it "assigns @products" do
-      category = Category.create name:'Laptop'
-      get :index, params: { product: { product_name: 'Macbook 2015', quantity: 50, unit_price: 200, category_id: category.id}}
-      expect(assigns(:products))
-    end
+   it "renders the index view" do
+    get :index
+    expect(response).to render_template(:index)
+  end
+end
 
-     it "renders the index view" do
-      get :index
-      expect(response).to render_template(:index)
+describe "#new" do
+  it "assigns a new product to @products" do
+    get :new
+    expect(response).to have_http_status(:success)
+  end
+
+   it "renders the new view" do
+    get :new
+    expect(response).to render_template(:new)
+  end
+end
+
+  describe "#create" do
+    context "success" do
+      it "create a presisted product" do
+        category = Category.create name: 'Laptop'
+        post :create, params: { product: { product_name: 'Macbook 2015', quantity: 50, unit_price: 200, category_id: category.id}}
+        expect(Product.count).to eql(1)
+      end
+
+      it "redirect_to products index pages" do
+        category = Category.create name: 'Laptop'
+        post :create, params: { product: { product_name: 'Macbook 2015', quantity: 50, unit_price: 200, category_id: category.id}}
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(products_path)
+      end
+    end
+    
+    context "failed" do
+      it "does not create a new product in database" do
+        category = Category.create name: 'Laptop'
+        post :create, params: { product: { product_name: '', quantity: '', unit_price: '', category_id: ''}}
+        expect(Product.count).to eql(0)
+      end
+
+      it "render products new pages" do
+        category = Category.create name: 'Laptop'
+        post :create, params: { product: { product_name: '', quantity: '', unit_price: '', category_id: ''}}
+        expect(response).to render_template(:new)
+      end
     end
   end
 end
