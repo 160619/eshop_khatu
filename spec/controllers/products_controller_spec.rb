47a31629f17b4853a390bce0b2ad0e52f95137b3
KHATU <NGUYENBAOKHATU@GMAIL.com>
2020-08-30 14:24:42 +0700

    Refactor

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,33 +2,33 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
 
-describe "#index" do
-  it "assigns products" do
-    category = Category.create(name: 'Laptop')
-    Product.create(product_name: 'Macbook 2015', quantity: 50, unit_price: 200, category_id: category.id)
-    get :index
-    products = assigns(:products)
-    expect(products.size).to eql(1)
+  describe "#index" do
+    it "assigns products" do
+      category = Category.create(name: 'Laptop')
+      Product.create(product_name: 'Macbook 2015', quantity: 50, unit_price: 200, category_id: category.id)
+      get :index
+      products = assigns(:products)
+      expect(products.size).to eql(1)
+    end
+
+     it "renders the index view" do
+      get :index
+      expect(response).to render_template(:index)
+    end
   end
 
-   it "renders the index view" do
-    get :index
-    expect(response).to render_template(:index)
-  end
-end
+  describe "#new" do
+    it "assigns a new product to @products" do
+      get :new
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:new)
+    end
 
-describe "#new" do
-  it "assigns a new product to @products" do
-    get :new
-    expect(response).to have_http_status(:success)
-    expect(response).to render_template(:new)
+     it "befor create a presisted product" do
+      get :new
+      expect(assigns(:product)).to be_a_kind_of(Product)
+    end
   end
-
-   it "befor create a presisted product" do
-    get :new
-    expect(assigns(:product)).to be_a_kind_of(Product)
-  end
-end
 
   describe "#create" do
     context "success" do
