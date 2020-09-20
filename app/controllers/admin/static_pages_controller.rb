d6267e574fc778d33f2e416014d545ea30538192
KHATU <NGUYENBAOKHATU@GMAIL.com>
2020-09-20 10:08:19 +0700

    Refactor code

--- app/controllers/admin/static_pages_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Description/Explanation of Admin::StaticPagesController class
-
-class Admin::StaticPagesController < AdminController
-  before_action :authenticate_user!
-
-  def home; end
-end
--- app/controllers/admin_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Description/Explanation of AdminController class
-
-class AdminController < ActionController::Base
-  before_action :authenticate_user!
-end
